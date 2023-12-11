48527af92c792a5aafa696bbeddd4bb4008502e3
ariameli <ariadne.melissargos@heig-vd.ch>
2023-12-11 14:43:22 +0100

    modified the base url

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 // "http://localhost:8888/comem-archidep-php-todo-exercise/", then BASE_URL
 // should be "/comem-archidep-php-todo-exercise/". If you are accessing the
 // application at "http://localhost:8888", then BASE_URL should be "/".
-define('BASE_URL', '/');
-
+//define('BASE_URL', '/');
+define('BASE_URL', getenv('TODOLIST_BASE_URL') ?: '/');
 // Database connection parameters.
 define('DB_USER', 'todolist');
 define('DB_PASS', 'chAngeMeN0w!');
